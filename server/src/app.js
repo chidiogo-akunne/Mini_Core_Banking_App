be8ee6a13081e4516d81eca1e58de926fe1f3a6c
chidiogo-akunne <chidiogoakunne@gmail.com>
2019-12-09 15:47:27 +0100

    add graphql and enable graphiql

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import path from 'path';
 import cookieParser from'cookie-parser';
 import logger from'morgan';
+import graphqlHTTP from 'express-graphql';
 
 import indexRouter from'./routes/index';
 import usersRouter from'./routes/users';
@@ -21,6 +22,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('/graphql', graphqlHTTP({
+  schema, 
+  graphiql: true
+}))
 
 // catch 404 and forward to error handler
 app.use(function(_req, _res, next) {
