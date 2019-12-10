a0bcdb21bf70f270a8412a8287893315dab687ae
chidiogo-akunne <chidiogoakunne@gmail.com>
2019-12-10 23:48:16 +0100

    create nav logo

---
+++ client/src/Components/NavLogo/NavaLogo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function NavaLogo() {
+    return (
+        <div>
+            <div className="admin-nav-logo">
+      Mini <span>|>Bank</span>
+    </div>
+        </div>
+    )
+}
