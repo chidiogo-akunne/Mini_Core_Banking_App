20670ee1e4a422bb6aedfa9e3be8f04f972439b8
chidiogo-akunne <chidiogoakunne@gmail.com>
2019-12-10 17:01:05 +0100

    create input component

--- client/src/Components/Inputs/Input.js
+++ client/src/Components/Inputs/Input.js
@@ -3,7 +3,14 @@ import React from 'react'
 export default function Input(props) {
     return (
 <div className={props.className}>
-    <input type={props.type} className="form-control" id="exampleInputEmail1"/>
+    <input type={props.type} placeholder={props.placeholder} style={Inputstyle} onChange={props.onChange} className="form-control" id={props.id} name={props.name} password={props.password} values={props.value}/>
 </div>
     )
+}
+
+const Inputstyle = {
+    borderTop: "0",
+    borderLeft: "0",
+    borderRight: "0",
+    borderRadius: "0"
 }
