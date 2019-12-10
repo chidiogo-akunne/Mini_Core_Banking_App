93bff6c54f38af29f41a71d86426ace538337266
chidiogo-akunne <chidiogoakunne@gmail.com>
2019-12-10 17:00:52 +0100

    create button component

--- client/src/Components/Buttons/Buttons.js
+++ client/src/Components/Buttons/Buttons.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function Buttons(props) {
-    return (
-        <div>
-            <button type="button" className={props.className}>{props.children}</button>
-        </div>
-    )
+  return (
+    <div>
+      <button type="submit" className={props.className} onClick={props.onClick}>
+        {props.children}
+      </button>
+    </div>
+  );
 }
