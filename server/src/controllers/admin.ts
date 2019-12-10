67776188d9fb3154e7b3ad17812b34dcc20643a4
chidiogo-akunne <chidiogoakunne@gmail.com>
2019-12-10 04:08:47 +0100

    create admin function

--- server/src/controllers/admin.ts
+++ server/src/controllers/admin.ts
@@ -1,17 +1,33 @@
-import Admin from '../models/admin';
-import bcrypt from 'bcrypt'
+import Admin from "../models/admin";
+import bcrypt from "bcrypt";
 
-export async function loginAdmin(username: string, password: string){
-    const findAdmin = await Admin.find({username});
-    if(!findAdmin.length){
-    //    return {status: 404, message: 'Invalid username or password'}; 
-    throw new Error('Invalid username or password')
-    }
+export async function loginAdmin(username: string, password: string) {
+  const findAdmin = await Admin.findOne({ username: username });
+  if (!findAdmin) {
+    //    return {status: 404, message: 'Invalid username or password'};
+    throw new Error("Invalid username or password");
+  }
 
-    const comparePassword = await bcrypt.compare(password, password);
-    if(!comparePassword){
-        // return { status: 404, message: 'Invalid username or password'};
-        throw new Error('Invalid username or password ')
+  const comparePassword = await bcrypt.compare(password, findAdmin.password);
+  if (!comparePassword) {
+    // return { status: 404, message: 'Invalid username or password'};
+    throw new Error("Invalid username or password ");
+  }
+  return { status: 200, message: "Login Succesful", payload: findAdmin };
+}
+
+export async function createAdmin(firstname: string, lastname: string, email: string, username: string, password: string){
+    const alreadyExists = await Admin.findOne({email: email});
+    if(alreadyExists){
+        throw new Error("Admin already exists");
     }
-    return {staus: 200, message: 'Login Succesful'}
+    const response = await Admin.create({
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        username: username,
+        password: password,
+    }) 
+
+    return {status: 200, message: "Admin has been added successfully", payload: response}
 }
