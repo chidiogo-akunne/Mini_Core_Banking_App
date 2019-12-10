5a7f0da809f461d7fdb598dd55db7ab12548af98
chidiogo-akunne <chidiogoakunne@gmail.com>
2019-12-10 07:52:44 +0100

    validation for creation of account

--- server/src/validation/validation.ts
+++ server/src/validation/validation.ts
@@ -1,5 +1,7 @@
 import Joi from '@hapi/joi';
 
+const phoneRegex = /^(\+?234[789][01]\d{8})$|^(0[789][01]\d{8})$/;
+
 const createAdminSchema = Joi.object({
     firstname: Joi
     .string()
@@ -27,6 +29,12 @@ lastname: Joi
     .lowercase()
     .trim()
     .required(), 
+    password: Joi.string()
+    .min(8)
+    .max(255)
+    .trim()
+    .required()
+    .label('Password does not meet requirements'),
 })
 
 const loginAdminSchema = Joi.object({
@@ -45,5 +53,88 @@ password: Joi.string()
     .required()
     .label('Password does not meet requirements'),
 });
+
+const openAccountSchema = Joi.object({
+    firstname: Joi
+    .string()
+    .min(3)
+    .max(40)
+    .trim()
+    .lowercase()
+    .required(),
+lastname: Joi
+    .string()
+    .min(3)
+    .max(40)
+    .trim()
+    .lowercase()
+    .required(),
+  email: Joi
+    .string()
+    .trim()
+    .email(),
+  username: Joi
+    .string()
+    .alphanum()
+    .min(4)
+    .max(40)
+    .lowercase()
+    .trim()
+    .required(), 
+    gender: Joi
+    .string()
+    .min(3)
+    .max(10)
+    .trim()
+    .lowercase()
+    .required(),
+    nationality: Joi
+    .string()
+    .min(3)
+    .max(20)
+    .trim()
+    .lowercase()
+    .required(),
+    maritaStatus: Joi
+    .string()
+    .min(3)
+    .max(20)
+    .trim()
+    .lowercase()
+    .required(),
+    religion: Joi
+    .string()
+    .min(3)
+    .max(20)
+    .trim()
+    .lowercase(),
+    dateOfBirth: Joi
+    .string()
+    .min(3)
+    .max(40)
+    .trim()
+    .lowercase()
+    .required(),
+    employmentStatus: Joi
+    .string()
+    .min(3)
+    .max(10)
+    .trim()
+    .lowercase(),
+    phoneNumber: Joi
+    .string()
+    .min(3)
+    .max(40)
+    .trim()
+    .lowercase()
+    .pattern(phoneRegex)
+    .required(),
+    password: Joi.string()
+    .min(8)
+    .max(255)
+    .trim()
+    .required()
+    .label('Password does not meet requirements'),
+}) 
 
 export {createAdminSchema, loginAdminSchema}
