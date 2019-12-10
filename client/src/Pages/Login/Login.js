df3121deac3506f6ad0c6dbf8ba0664d6431c5af
chidiogo-akunne <chidiogoakunne@gmail.com>
2019-12-10 17:01:55 +0100

    create login page

---
+++ client/src/Pages/Login/Login.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Cards from "../../Components/Cards/Card";
+import Inputs from "../../Components/Inputs/Input";
+import Buttons from "../../Components/Buttons/Buttons";
+import "./Login.css";
+import { withRouter } from "react-router-dom";
+
+function Login(props) {
+  const [response, setResponse] = useState("");
+  const [values, setValues] = useState({
+    username: "",
+    password: ""
+  });
+
+  function handleChange(e) {
+    e.preventDefault();
+    setValues({ ...values, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios({
+      method: "POST",
+      url: `http://localhost:5000/admin/login`,
+      data: values
+    })
+      .then(response => {
+        console.log(response.data.data.message);
+        setResponse(response.data.data.message);
+        localStorage.setItem("token", response.data.data.token);
+        props.history.push("/admin/dashboard");
+      })
+      .catch(err => {
+        setResponse(err.response.data.message);
+      });
+  }
+  return (
+    <div>
+      <Cards Cardstyle={cardStyle}>
+        <h3>MiniBank.Com</h3>
+        <p>{response}</p>
+        <form onSubmit={handleSubmit}>
+          <Inputs
+            placeholder="Username"
+            type="text"
+            id="LoginUsername"
+            onChange={handleChange}
+            value={values.username}
+            name="username"
+          />
+          <Inputs
+            placeholder="Password"
+            type="password"
+            id="LoginPassword"
+            onChange={handleChange}
+            value={values.password}
+            name="password"
+          />
+          <Buttons
+            className="loginButton btn btn-success"
+            onClick={handleSubmit}
+          >
+            Login
+          </Buttons>
+          <h6>Do you want to signup? </h6>
+        </form>
+      </Cards>
+    </div>
+  );
+}
+
+const cardStyle = {
+  width: "60%",
+  border: "none",
+  boxShadow: "4px 10px 15px #888",
+  margin: "15% auto"
+};
+
+export default withRouter(Login);
