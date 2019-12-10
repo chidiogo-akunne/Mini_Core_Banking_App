483845807c5ae854dc89d991b3d93ffbab6c339f
chidiogo-akunne <chidiogoakunne@gmail.com>
2019-12-10 11:22:28 +0100

    create and close account

--- server/src/routes/admin.ts
+++ server/src/routes/admin.ts
@@ -1,9 +1,80 @@
 import express from 'express';
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(_req, res, _next) {
-    res.send('respond with a resource');
+import {createAdminSchema, loginAdminSchema, openAccountSchema} from '../validation/validation'
+import {loginAdmin, createAdmin} from '../controllers/admin';
+import {openAccount, closeAccount} from '../controllers/Account'
+
+/* admin login. */
+router.post('/login', async(req, res) => {
+    const {error} = loginAdminSchema.validate(req.body, {
+        abortEarly: false,
+        stripUnknown: true,
+    })
+    if(error){
+        res.status(400).json({
+            message: 'Error'
+        })
+    }
+
+    const{username, password} = req.body;
+    try{
+        const login = await loginAdmin(username, password);
+        return res.status(200).json({data: login})
+    } catch (err){
+        return res.status(500).json({message: err.message})
+    }
 });
+
+router.post('/signup', async(req, res) => {
+    const {error} = createAdminSchema.validate(req.body, {
+        abortEarly: false,
+        stripUnknown: true,
+    })
+    if(error){
+        res.status(400).json({
+            message: 'Error'
+        })
+    }
+
+    const{username, password, firstname, lastname, email} = req.body;
+    try{
+        const signup = await createAdmin(username, password, firstname, lastname, email);
+        return res.status(200).json({data: signup})
+    } catch (err){
+        return res.status(500).json({message: err.message})
+    }
+})
+
+router.post('/createaccount', async(req, res) => {
+    const {error} = openAccountSchema.validate(req.body, {
+        abortEarly: false,
+        stripUnknown: true,
+    })
+    if(error){
+        res.status(400).json({
+            message: 'Error'
+        })
+    }
+
+    const{accountOpeningDetails} = req.body;
+    try{
+        const signup = await openAccount(accountOpeningDetails);
+        return res.status(200).json({data: signup})
+    } catch (err){
+        return res.status(500).json({message: err.message})
+    }
+})
+
+router.patch('/closeaccount', async(req, res) => {
+  
+    const{accoundID} = req.body;
+    try{
+        const signup = await closeAccount(accoundID);
+        return res.status(200).json({data: signup})
+    } catch (err){
+        return res.status(500).json({message: err.message})
+    }
+})
 
 export default router;
